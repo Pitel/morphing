ec0518875942fbedf044535f61660ca8539b585d
Bc. Jan Kaláb <pitlicek@gmail.com>
2011-04-23 18:31:46 +0200

    Body jsou inty (krome vyjimky kterou potrebuje getPerspectiveTransform), maskovani

--- cvmorph.cpp
+++ cvmorph.cpp
@@ -6,17 +6,23 @@ using namespace std;
 using namespace cv;
 
 void morph(const Mat &img1, const Mat &img2, Mat &out, const Mat grid1, const Mat grid2, const float morp) {
-	Point2f tile1[4];
-	tile1[0] = grid1.at<Point2f>(0, 0);
-	tile1[1] = grid1.at<Point2f>(0, 1);
-	tile1[2] = grid1.at<Point2f>(1, 0);
-	tile1[3] = grid1.at<Point2f>(1, 1);
+	Point tile1[4];
+	tile1[0] = grid1.at<Point>(0, 0);
+	tile1[1] = grid1.at<Point>(0, 1);
+	tile1[2] = grid1.at<Point>(1, 1);
+	tile1[3] = grid1.at<Point>(1, 0);
+	Point2f tile1f[4] = {tile1[0], tile1[1], tile1[2], tile1[3]};
 	
-	Point2f tile2[4];
-	tile2[0] = grid2.at<Point2f>(0, 0);
-	tile2[1] = grid2.at<Point2f>(0, 1);
-	tile2[2] = grid2.at<Point2f>(1, 0);
-	tile2[3] = grid2.at<Point2f>(1, 1);
+	Point tile2[4];
+	tile2[0] = grid2.at<Point>(0, 0);
+	tile2[1] = grid2.at<Point>(0, 1);
+	tile2[2] = grid2.at<Point>(1, 1);
+	tile2[3] = grid2.at<Point>(1, 0);
+	Point2f tile2f[4] = {tile2[0], tile2[1], tile2[2], tile2[3]};
 	
-	warpPerspective(img1, out, getPerspectiveTransform(tile1, tile2), out.size(), QUALITY, BORDER_TRANSPARENT);
+	warpPerspective(img1, out, getPerspectiveTransform(tile1f, tile2f), out.size(), QUALITY, BORDER_TRANSPARENT);
+	
+	Mat mask(out.size(), out.type());
+	fillConvexPoly(mask, tile1, 4, CV_RGB(255, 255, 255), CV_AA, 0);
+	out &= mask;
 }
--- morphing.cpp
+++ morphing.cpp
@@ -12,28 +12,28 @@ using namespace cv;
 int main(int argc, char *argv[]) {
 	const char *imagename = argc > 1 ? argv[1] : "Lenna.png";
 	cout << "Loading " << imagename << endl;
-	Mat img = Mat_<float>(imread(imagename));
+	Mat img = imread(imagename);
 	if (!img.data) {
 		return EXIT_FAILURE;
 	}
 	
-	Mat out(img.rows, img.cols, DataType<float>::type);
-	Mat grid1(2, 2, DataType<Point2f>::type);
-	Mat grid2(2, 2, DataType<Point2f>::type);
+	Mat out(img.size(), img.type());
+	Mat grid1(2, 2, DataType<Point>::type);
+	Mat grid2(2, 2, DataType<Point>::type);
 	
-	grid1.at<Point2f>(0, 0) = Point2f(150, 150);
-	grid1.at<Point2f>(0, 1) = Point2f(250, 100);
-	grid1.at<Point2f>(1, 0) = Point2f(100, 300);
-	grid1.at<Point2f>(1, 1) = Point2f(300, 250);
+	grid1.at<Point>(0, 0) = Point(150, 150);
+	grid1.at<Point>(0, 1) = Point(250, 100);
+	grid1.at<Point>(1, 0) = Point(100, 300);
+	grid1.at<Point>(1, 1) = Point(300, 250);
 	
-	grid2.at<Point2f>(0, 0) = Point2f(100, 100);
-	grid2.at<Point2f>(0, 1) = Point2f(300, 100);
-	grid2.at<Point2f>(1, 0) = Point2f(100, 300);
-	grid2.at<Point2f>(1, 1) = Point2f(300, 300);
+	grid2.at<Point>(0, 0) = Point(100, 100);
+	grid2.at<Point>(0, 1) = Point(300, 100);
+	grid2.at<Point>(1, 0) = Point(100, 300);
+	grid2.at<Point>(1, 1) = Point(300, 300);
 	
 	morph(img, img, out, grid1, grid2, 0.5);
-	imshow ("input", Mat_<uchar>(img));
-	imshow ("output", Mat_<uchar>(out));
+	imshow("input", img);
+	imshow("output", out);
 	
 	waitKey();
 	return EXIT_SUCCESS;
